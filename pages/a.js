41627baabd6ae0980b3b386b859f80c3a3fdf8a1
折田幸夫 <3204704@gmail.com>
2020-03-07 14:45:10 +0900

    testing

--- pages/a.js
+++ pages/a.js
@@ -1,16 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 
+const Layout = styled.div``;
+
 const StyledH1 = styled.h1`
+  display: flex;
+  justify-content: center;
   padding: 10px;
+`;
+
+const StyledText = styled.p`
+  display: flex;
+  justify-content: center;
+  height: 200px;
 `;
 
 const firstPage = () => {
   return (
-    <>
+    <Layout>
       <StyledH1>aのページって話し</StyledH1>
-      <div>styled-component導入完了です〜〜</div>
-    </>
+      <StyledText>styled-component導入完了です〜〜</StyledText>
+    </Layout>
   );
 };
 
