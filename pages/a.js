b4131656f30783af973c255ccfce9ec5dba2f636
折田幸夫 <3204704@gmail.com>
2020-03-07 14:37:05 +0900

    feat styled-components

--- pages/a.js
+++ pages/a.js
@@ -1,1 +1,17 @@
-export default () => <div>a</div>
+import React from "react";
+import styled from "styled-components";
+
+const StyledH1 = styled.h1`
+  padding: 10px;
+`;
+
+const firstPage = () => {
+  return (
+    <>
+      <StyledH1>aのページって話し</StyledH1>
+      <div>styled-component導入完了です〜〜</div>
+    </>
+  );
+};
+
+export default firstPage;
